e2b1ce11b2df4802adee44cbdf4ac822a2e0d816
MayAlexis <al1701007@upy.edu.mx>
2019-06-28 11:17:28 -0500

    the feactures of the sensors and motors were added according the datasheets

---
+++ controllers/onmiwheel/build/release/onmiwheel.d
@@ -0,0 +1,4 @@
+build/release/onmiwheel.o: onmiwheel.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h
